e30a4ae1e5aa85ee894dfdea5042ea44b910f7b6
Sly-Zapesti <133956160+Sly-Zapesti@users.noreply.github.com>
2023-05-18 11:08:14 +0200

    Add files via upload

---
+++ sGangmenu/config.lua
@@ -0,0 +1,22 @@
+
+Config = {
+
+   
+----------------------------------------------------Gestion Gang----------------------------------------------------
+job = {
+   {job2 = "Vagos"},
+   {job2 = "Ballas"},
+   {job2 = "Families"},
+   {job2 = "Mara"},
+   {job2 = "Mafia"},
+   {job2 = "blanchi"},
+   {job2 = "CosaNostra"},
+   {job2 = "hitman"},
+},  
+
+--Ligne à ajouter pour mettre la permission à un gang : {job2 = "nom du gang"}
+
+----------------------------------------------------Gestion menu----------------------------------------------------
+menuinformation = true, --Activer ou non le menu information
+
+}
